f8a749695f8bdcf3ca1579e6914500a5ff9d3e67
Bert Manke <bert.manke@gmail.com>
2026-05-13 17:34:40 +0200

    "script.js: added function renderNotes

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 
 //notizen anzeigen lassen
+//ich brauche notizen
+let notes = [`banana`, `rasen mähen`];
+
+//wann werden sie angezeigt
+function renderNotes(){
+    //ich muss definieren, wo die notizen angezeigt werden sollen
+    let contentRef = document.getElementById(`content`);
+
+    contentRef.innerHTML = notes;
+}
+
+
+
+
+
 
 //notizen hinzufügen
 //notizen löschen
